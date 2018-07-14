3c4a1b52d13f571e360ed43676ea73dd983c1fc7
Eduardo Dobay <edudobay@gmail.com>
2018-07-14 12:50:29 -0300

    Reorganize core/resources

---
+++ core/resource_tree.go
@@ -0,0 +1,42 @@
+package core
+
+type Resources struct {
+	stack *resourcesStack
+}
+
+func (rs *Resources) AddDisposable(d Disposable) {
+	rs.stack = rs.stack.Append(d)
+}
+
+func (rs *Resources) Init(r Resource) error {
+	err := r.Init()
+	if err != nil {
+		return err
+	}
+	rs.stack = rs.stack.Append(r)
+	return nil
+}
+
+func (rs *Resources) Dispose() {
+	var d Disposable
+	for stack := rs.stack; stack != nil; {
+		d, stack = stack.Pop()
+		d.Dispose()
+	}
+	rs.stack = nil
+}
+
+//
+
+type resourcesStack struct {
+	top  Disposable
+	rest *resourcesStack
+}
+
+func (stack *resourcesStack) Append(d Disposable) *resourcesStack {
+	return &resourcesStack{d, stack}
+}
+
+func (stack *resourcesStack) Pop() (Disposable, *resourcesStack) {
+	return stack.top, stack.rest
+}
--- core/resources.go
+++ core/resources.go
@@ -13,57 +13,11 @@ type Resource interface {
 	Disposable
 }
 
-type resourcesStack struct {
-	top  Disposable
-	rest *resourcesStack
-}
-
-func (stack *resourcesStack) Append(d Disposable) *resourcesStack {
-	return &resourcesStack{d, stack}
-}
-
-func (stack *resourcesStack) Pop() (Disposable, *resourcesStack) {
-	return stack.top, stack.rest
-}
-
-type Resources struct {
-	stack *resourcesStack
-}
-
-func (rs *Resources) AddDisposable(d Disposable) {
-	rs.stack = rs.stack.Append(d)
-}
-
-func (rs *Resources) Init(r Resource) error {
-	err := r.Init()
-	if err != nil {
-		return err
-	}
-	rs.stack = rs.stack.Append(r)
-	return nil
-}
-
-func (rs *Resources) Dispose() {
-	var d Disposable
-	for stack := rs.stack; stack != nil; {
-		d, stack = stack.Pop()
-		d.Dispose()
-	}
-	rs.stack = nil
-}
+//
 
 type SimpleResource struct {
 	OnInit    func() error
 	OnDispose func()
-}
-
-func DisposableResource(dispose func()) Resource {
-	return SimpleResource{
-		OnInit: func() error {
-			return nil
-		},
-		OnDispose: dispose,
-	}
 }
 
 func (d SimpleResource) Init() error {
@@ -72,4 +26,15 @@ func (d SimpleResource) Init() error {
 
 func (d SimpleResource) Dispose() {
 	d.OnDispose()
+}
+
+//
+
+func DisposableResource(dispose func()) Resource {
+	return SimpleResource{
+		OnInit: func() error {
+			return nil
+		},
+		OnDispose: dispose,
+	}
 }
